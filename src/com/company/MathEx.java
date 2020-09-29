e255b986b2386bf227f790bc7f43ce9e88bb580c
rdonati22 <racheldonati@billericak12.com>
2020-09-28 21:50:14 -0400

    second version

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -2,15 +2,20 @@ package com.company;
 
 public class MathEx {
     public static void main(String[] args) {
-        swap(9876);
+        int num = swap(9876);
+        System.out.println(num);
+        //timeLeft(2,35,6,38);
     }
-    public static void swap(int x) {
+    public static int swap(int x) {
         int y = (x/10)%10;
         int z = ((x*10)/10)%10;
         int a = x/100;
-        System.out.print(a);
+        return a;
+        //return z;
+        //return y;
+        /*System.out.print(a);
         System.out.print(z);
-        System.out.print(y);
+        System.out.print(y);*/
         /*if (a!=0){
             System.out.print(a);
             System.out.print(z);
@@ -20,5 +25,18 @@ public class MathEx {
             System.out.print(z);
             System.out.print(y);
         }*/
+    }
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+        int h = depHour - curHour;
+        int m = depMin - curMin;
+        if (m<0){
+            h--;
+            m=60-curMin+depMin;
+        }
+        System.out.println("Train leaves in "+h+" hours and "+m+" minutes!!");
+    }
+    public static void dayOfWeek(int firstDay, int date){
+                                    //%7
+        System.out.println("");
     }
 }
