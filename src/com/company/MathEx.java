a242cdcb5bba5582043cac41d5ef4fcdbb6bad62
rdonati22 <racheldonati@billericak12.com>
2020-09-29 13:43:23 -0400

    third version

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -2,29 +2,18 @@ package com.company;
 
 public class MathEx {
     public static void main(String[] args) {
-        int num = swap(9876);
+        /*int num = swap(9876);
         System.out.println(num);
-        //timeLeft(2,35,6,38);
+        timeLeft(4,35,6,38);*/
+        int num2 = dayOfWeek(0,14);
+        System.out.println("Day of Week: "+num2);
     }
     public static int swap(int x) {
         int y = (x/10)%10;
-        int z = ((x*10)/10)%10;
-        int a = x/100;
-        return a;
-        //return z;
-        //return y;
-        /*System.out.print(a);
-        System.out.print(z);
-        System.out.print(y);*/
-        /*if (a!=0){
-            System.out.print(a);
-            System.out.print(z);
-            System.out.print(y);
-        }
-        else{
-            System.out.print(z);
-            System.out.print(y);
-        }*/
+        int z = ((x*10)/10)%10*10;
+        int a = x/100*100;
+        int h = a+z+y;
+        return h;
     }
     public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
         int h = depHour - curHour;
@@ -35,8 +24,11 @@ public class MathEx {
         }
         System.out.println("Train leaves in "+h+" hours and "+m+" minutes!!");
     }
-    public static void dayOfWeek(int firstDay, int date){
-                                    //%7
-        System.out.println("");
+    public static int dayOfWeek(int firstDay, int date){
+        //int x = (date-firstDay+8)%7;
+        //int x = date%7-1+firstDay-1+7+date%7;
+        int x = date%7+1-firstDay;
+        //int x = date%7+firstDay-1;                                    //%7
+        return x;
     }
 }
